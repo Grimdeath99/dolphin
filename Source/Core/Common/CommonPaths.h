24260927f11383794a6efa0e50e6f738b38a3110
iwubcode <iwubcode@users.noreply.github.com>
2023-11-03 23:35:05 -0500

    Common: add define for graphics mod editor directory name

--- Source/Core/Common/CommonPaths.h
+++ Source/Core/Common/CommonPaths.h
@@ -94,6 +94,7 @@
 #define DYNAMICINPUT_DIR "DynamicInputTextures"
 #define GRAPHICSMOD_DIR "GraphicMods"
 #define WIISDSYNC_DIR "WiiSDSync"
+#define GRAPHICSMODEDITOR_DIR "GraphicsModEditor"
 
 // This one is only used to remove it if it was present
 #define SHADERCACHE_LEGACY_DIR "ShaderCache"
